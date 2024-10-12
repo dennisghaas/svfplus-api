61c4215387576a0860d5091815407550dc5b4e80
Dennis Haas <haasde@netrtl.de>
2024-10-13 01:35:31 +0200

    update cors

--- server.js
+++ server.js
@@ -12,17 +12,11 @@ const blockedUsersRoutes = require('./routes/blockedUserRoutes')
 app.use(express.json());
 
 // Konfiguriere CORS
-const corsOptions = {
-  origin: (req, callback) => {
-    const isDev = process.env.NODE_ENV === 'development'; // Überprüfe, ob die Umgebung Entwicklung ist
-    const origin = isDev ? process.env.CORS_ORIGIN_DEV : process.env.CORS_ORIGIN_PROD;
-    callback(null, origin); // Erlaube den Zugriff basierend auf der Umgebung
-  },
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
+}));
 
 // Routen verwenden
 app.use('/api/users', userRoutes);
