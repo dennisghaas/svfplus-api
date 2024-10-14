ecfb1087c3a65fcf4b8e6f823d55a0cc6ae67ba6
Dennis Haas <haasde@netrtl.de>
2024-10-14 18:05:25 +0200

    update branch

--- testConnection.js
+++ testConnection.js
@@ -1,9 +1,17 @@
-const sequelize = require('./config/database');
+require("dotenv").config();
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Verbindung zur Datenbank erfolgreich!');
-    })
-    .catch(err => {
-        console.error('Fehler bei der Verbindung zur Datenbank:', err);
-    });
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+    const result = await sql`SELECT version()`;
+    const { version } = result[0];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
