894c9b792f1c204642d695f410f4311150d4d185
Dennis Haas <haasde@netrtl.de>
2024-10-15 12:18:35 +0200

    fix data base connection

--- config/database.js
+++ config/database.js
@@ -1,27 +1,27 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 
 // Umgebungsabhängige Konfiguration
 const env = process.env.NODE_ENV || 'development';
 
 // Datenbankkonfiguration basierend auf der Umgebung
 const config = {
-  development: {
-    username: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || null,
-    database: process.env.MYSQL_DATABASE || 'development_db',
-    host: process.env.MYSQL_HOST || 'localhost',
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-  },
-  production: {
-    username: process.env.MYSQL_USER_PROD,
-    password: process.env.MYSQL_PASSWORD_PROD,
-    database: process.env.MYSQL_DATABASE_PROD,
-    host: process.env.MYSQL_HOST_PROD,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-  },
+    development: {
+        username: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || null,
+        database: process.env.MYSQL_DATABASE || 'development_db',
+        host: process.env.MYSQL_HOST || 'localhost',
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+    },
 };
 
 // Wähle die passende Konfiguration je nach Umgebung
@@ -33,9 +33,9 @@ const sequelize = new Sequelize(
     currentConfig.username,
     currentConfig.password,
     {
-      host: currentConfig.host,
-      dialect: currentConfig.dialect,
-      port: currentConfig.port,
+        host: currentConfig.host,
+        dialect: currentConfig.dialect,
+        port: currentConfig.port,
     }
 );
 
